08cd56ea20d3751e2953c231d244f23e5e93840e
Jess <jessica.louvel@gmail.com>
2023-08-14 08:08:03 +0200

    fix experiecnes fetch

--- app/components/Experiences.tsx
+++ app/components/Experiences.tsx
@@ -20,7 +20,7 @@ export default  function Experiences() {
 	useEffect(() => {
 	  const getExperiences = async () => {
 		try {
-		  const response = await fetch('http://127.0.0.1:1337/api/services', { cache: "no-store" });
+		  const response = await fetch('http://127.0.0.1:1337/api/experiences', { cache: "no-store" });
 		  const data = await response.json();
 			setData(data.data);
 			console.log(data)
@@ -37,19 +37,20 @@ export default  function Experiences() {
 		<section className="experiences-section px-6 md:px-10 lg:px-16 xl:px-32">
 			{data?.map((item) => (
 				<div className="pt-32 lg:flex lg:flex-row pb-10">
-					<div className="lg:w-4/12">
+					<div className="lg:w-6/12">
 						<div className="flex flex-col lg:flex-row lg:items-baseline">
-							<div className="lg:w-2/12">
-								<p className={`text-[#17515c] dark:text-[#c0ccbb] ${yeseva.className}`}>{item.attributes.date}</p>
+							<div className="flex flex-row  lg:flex-col  lg:w-1/6">
+								<p className={`text-[#17515c] dark:text-[#c0ccbb] after:content-['-'] after:pl-4 lg:after:content-[''] ${yeseva.className}`}>{item.attributes.dateStart}</p>
+								<p className={`text-[#17515c] dark:text-[#c0ccbb] lg:pt-0 lg:mt-0 pl-4 lg:pl-0 ${yeseva.className}`}>{item.attributes.dateEnd}</p>
 							</div>
 
-							<div className="lg:w-10/12">
+							<div className="lg:pl-4 lg:w-5/6">
 								<h4 className="dark:text-[#E7E6E2]">{item.attributes.jobTitle}</h4>
 								<h6 className="dark:text-[#E7E6E2]">{item.attributes.company}</h6>
 							</div>
 						</div>
 					</div>
-					<div className=" lg:w-8/12 mt-10 lg:mt-0 mb-30">
+					<div className=" lg:w-6/12 mt-10 lg:mt-0 mb-30">
 						<div className="text">
 							<p className="dark:text-[#999]">{item.attributes.description}</p>
 						</div>
