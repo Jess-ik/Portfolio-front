3450887cc8a668af01a79e43d601a8c084f7a7ac
Jess <jessica.louvel@gmail.com>
2023-08-27 09:53:45 +0200

    edit for render

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {exportPathMap: function () {
-    return {
-      '/': { page: '/' },
-      // '/blog/nextjs': { page: '/blog/[post]/comment/[id]' },        // wrong
-      'app/projects/1': { page: 'app/projects/[slug]' }, // correct
-    }
-  },}
+const nextConfig = {}
 
 module.exports = nextConfig
