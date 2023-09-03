57230b1f47fdbbe8ecb6de0b2a90abdbb00fd72c
Jess <jessica.louvel@gmail.com>
2023-09-03 10:32:39 +0200

    fix contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -48,7 +48,7 @@ const Contact = () => {
 			// Vérifiez si le mail a été envoyé avec succès
 			if (response.status === 200) {
 				setMailSent(true); // Définit l'état pour afficher le message de confirmation
-			  }
+			}
 		} catch (error: unknown) {
 			if (error instanceof Error) {
 				setErrorContact(error);
@@ -64,10 +64,9 @@ const Contact = () => {
 		<>
 			<PageHead subtitle="Get in touch" title="Contact me" />
 			<section className="max-w-screen-2xl m-auto contact-sec px-10 md:px-16 lg:px-32 xl:px-72 text-center">
-				<div className=" py-20 md:py-32">
-					{mailSent ? (
+				{mailSent ? (
+					<div className=" py-20 md:py-32">
 						<div className="text-center text-[#17515c] dark:text-[#c0ccbb] font-bold">Mail sent successfully!</div>
-					) : (
 						<form id="contact-form" onSubmit={handleSubmit}>
 							<div className="flex flex-col md:flex-row md:flex-wrap">
 								<div className="w-full md:w-6/12 md:pr-4 ">
@@ -112,8 +111,55 @@ const Contact = () => {
 								</div>
 							</div>
 						</form>
-					)}
-				</div>
+					</div>
+				) : (
+					<div className=" py-20 md:py-32">
+						<form id="contact-form" onSubmit={handleSubmit}>
+							<div className="flex flex-col md:flex-row md:flex-wrap">
+								<div className="w-full md:w-6/12 md:pr-4 ">
+									<label className="form-group hidden" htmlFor="name">
+										Name
+									</label>
+									<input placeholder="Name" className="border-b border-[#0d2c32] dark:border-[#E7E6E2] text-#0d2c32 dark:text-white" type="text" id="name" name="name" value={modifiedData.name} onChange={handleChange} required />
+								</div>
+								<div className="w-full pt-6 md:pt-0 md:w-6/12 md:pl-4">
+									<label className="form-group hidden" htmlFor="email">
+										Email
+									</label>
+									<input placeholder="Email" className="form-group border-b border-[#0d2c32] dark:border-[#E7E6E2] text-#0d2c32 dark:text-white" type="email" id="email" name="email" value={modifiedData.email} onChange={handleChange} required />
+								</div>
+								<div className="w-full md:pt-10">
+									<div className="form-group">
+										<label className="form-group hidden" htmlFor="message">
+											Message
+										</label>
+										<textarea
+											placeholder="Message"
+											className="border-b border-[#0d2c32] dark:border-[#E7E6E2] text-#0d2c32 dark:text-white"
+											id="message"
+											name="message"
+											value={modifiedData.message}
+											onChange={handleMessageChange}
+											rows={4} // Vous pouvez ajuster ce nombre en fonction du nombre de lignes souhaité
+											required
+										/>{" "}
+									</div>
+								</div>
+
+								<div className="w-full">
+									<div className="text-center mx-auto">
+										<ReCAPTCHA className="text-center mx-auto" sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ""} onChange={handleRecaptchaChange} />
+									</div>
+									<div className="text-center mt-6">
+										<button type="submit" className="button dark:dark-button cursor-pointer" disabled={!recaptchaValue}>
+											Send Message
+										</button>
+									</div>
+								</div>
+							</div>
+						</form>
+					</div>
+				)}
 			</section>
 		</>
 	);
