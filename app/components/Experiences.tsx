a31c1a10d80fed8dfa2621c81f5d27b5e369eacc
Jess <jessica.louvel@gmail.com>
2023-09-18 11:21:28 +0200

    edit experiences

--- app/components/Experiences.tsx
+++ app/components/Experiences.tsx
@@ -4,16 +4,8 @@ import React, { useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import classNames from "classnames";
 
-// import { Yeseva_One } from "next/font/google";
-
 import { FaLocationDot } from "react-icons/fa6";
 import { BsChevronDown, BsChevronUp } from "react-icons/bs";
-
-// export const yeseva = Yeseva_One({
-// 	weight: ["400"],
-// 	style: ["normal"],
-// 	subsets: ["latin"],
-// });
 
 interface Experience {
 	id: string;
@@ -49,32 +41,25 @@ export default function Experiences() {
 		getExperiences();
 	}, []);
 
-	// const data = await getExperiences();
 	return (
 		<section className="max-w-screen-2xl m-auto experiences-section  pt-32 px-6 md:px-10 lg:px-16 xl:px-32">
 			{data?.map((item) => (
 				<div key={item.id} className=" lg:flex lg:flex-row pb-10 lg:pb-32 md:px-16">
+					{/* Job info */}
 					<div className="flex flex-col lg:w-1/3">
-						<div className="flex flex-row ">
-							{/* <p className={`text-[#17515c] dark:text-[#c0ccbb]  ${yeseva.className}`}>{item.attributes.date}</p> */}
-
-							<p className={`text-[#17515c] dark:text-[#c0ccbb]  yeseva`}>{item.attributes.date}</p>
-						</div>
-
-						<div className="">
-							<h4 className="dark:text-[#E7E6E2] pr-4">{item.attributes.jobTitle}</h4>
-							<h6 className="dark:text-[#E7E6E2]">{item.attributes.company}</h6>
-							<div className="flex flex-row flex-nowrap ">
-								<span className="text-[#17515c] dark:text-[#c0ccbb] mt-1 mr-1">
-									<FaLocationDot className="h-5 w-5" />
-								</span>
-								<h6 className="text-[#17515c] dark:text-[#c0ccbb] mt-1">{item.attributes.location}</h6>
-							</div>
+						<p className={`text-[#17515c] dark:text-[#c0ccbb]  yeseva`}>{item.attributes.date}</p>
+						<h4 className="dark:text-[#E7E6E2] pr-4">{item.attributes.jobTitle}</h4>
+						<h6 className="dark:text-[#E7E6E2]">{item.attributes.company}</h6>
+						<div className="flex flex-row flex-nowrap ">
+							<span className="text-[#17515c] dark:text-[#c0ccbb] mt-1 mr-1">
+								<FaLocationDot className="h-5 w-5" />
+							</span>
+							<h6 className="text-[#17515c] dark:text-[#c0ccbb] mt-1">{item.attributes.location}</h6>
 						</div>
 					</div>
-
+					{/* Job details */}
 					<div className="pt-6 lg:pt-0 lg:w-2/3 lg:pl-12 mb-30">
-						<button className="lg:hidden flex items-center text-[#0d2c32] dark:text-[#e7e6e2] mb-2" onClick={() => toggleJobDetails(item.id)}>
+						<button className="lg:hidden flex items-center button dark:dark-button cursor-pointer mb-2" onClick={() => toggleJobDetails(item.id)}>
 							Job details {openDetails === item.id ? <BsChevronUp className="ml-2" /> : <BsChevronDown className="ml-2" />}
 						</button>
 						<div className={classNames("collapsible-content text text-[#555] dark:text-[#999]", { open: openDetails === item.id })}>
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -26,7 +26,7 @@ const Contact = () => {
 	const [isMailSent, setIsMailSent] = useState(false);
 	// État pour stocker la valeur du reCAPTCHA
 	const [recaptchaValue, setRecaptchaValue] = useState<string | null>(null);
-	
+
 	// Fonction de gestion du changement d'input
 	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
 		const { name, value } = e.target;
@@ -35,7 +35,7 @@ const Contact = () => {
 			[name]: value,
 		}));
 	};
-	
+
 	// Fonction de gestion du changement du textarea
 	const handleMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
 		const { name, value } = e.target;
@@ -99,51 +99,52 @@ const Contact = () => {
 		<>
 			<PageHead subtitle="Get in touch" title="Contact me" />
 			<section className="max-w-screen-2xl m-auto contact-sec px-10 md:px-16 lg:px-32 xl:px-72 text-center">
-				
 				<div className="py-20 md:py-32">
 					{isMailSent ? (
 						<p className="text-[#17515c] dark:text-[#c0ccbb] pb-20">
 							Your message has been sent successfully, <br /> I'll get back to you shortly!
-						</p>) : ("")}
-						<form id="contact-form" onSubmit={handleSubmit}>
-							<div className="flex flex-col md:flex-row md:flex-wrap">
-								<div className="w-full md:w-6/12 md:pr-4 ">
-									<label className="form-group">
-										<input placeholder="Name" className={`border-b border-[#0d2c32] dark:border-[#E7E6E2] text-#0d2c32 dark:text-white ${errors.name ? "border-red-500" : ""}`} type="text" name="name" value={modifiedData.name} onChange={handleChange} />
-										{errors.name && <p className="text-red-500">{errors.name}</p>}
-									</label>
-								</div>
-								<div className="w-full pt-6 md:pt-0 md:w-6/12 md:pl-4">
-									<div className="form-group">
-										<input placeholder="Email" className={`border-b border-[#0d2c32] dark:border-[#E7E6E2] text-#0d2c32 dark:text-white ${errors.email ? "border-red-500" : ""}`} type="email" name="email" value={modifiedData.email} onChange={handleChange} />
-										{errors.email && <p className="text-red-500">{errors.email}</p>}
-									</div>
-								</div>
-								<div className="w-full md:pt-10">
-									<div className="form-group">
-										<textarea placeholder="Message" className={`border-b border-[#0d2c32] dark:border-[#E7E6E2] text-#0d2c32 dark:text-white ${errors.message ? "border-red-500" : ""}`} name="message" value={modifiedData.message} onChange={handleMessageChange} rows={4} />
-										{errors.message && <p className="text-red-500">{errors.message}</p>}
-									</div>
-								</div>
-								<div className="w-full flex justify-center m-auto">
-									<div className="text-center mt-6">
-										{isSending ? (
-											<div className="loader dark:text-[#c0ccbb]">Sending...</div>
-										) : (
-											<>
-												<ReCAPTCHA className="mb-6" sitekey={process.env.RECAPTCHA_SITE_KEY || ""} onChange={handleRecaptchaChange} />
-												<button type="submit" className="button dark:dark-button cursor-pointer">
-													Send message
-												</button>
-												{errorContact && <p className="text-red-500">{errorContact.message}</p>}
-											</>
-										)}
-									</div>
+						</p>
+					) : (
+						""
+					)}
+					<form id="contact-form" onSubmit={handleSubmit}>
+						<div className="flex flex-col md:flex-row md:flex-wrap">
+							<div className="w-full md:w-6/12 md:pr-4 ">
+								<label className="form-group">
+									<input placeholder="Name" className={`border-b border-[#0d2c32] dark:border-[#E7E6E2] text-#0d2c32 dark:text-white ${errors.name ? "border-red-500" : ""}`} type="text" name="name" value={modifiedData.name} onChange={handleChange} />
+									{errors.name && <p className="text-red-500">{errors.name}</p>}
+								</label>
+							</div>
+							<div className="w-full pt-6 md:pt-0 md:w-6/12 md:pl-4">
+								<div className="form-group">
+									<input placeholder="Email" className={`border-b border-[#0d2c32] dark:border-[#E7E6E2] text-#0d2c32 dark:text-white ${errors.email ? "border-red-500" : ""}`} type="email" name="email" value={modifiedData.email} onChange={handleChange} />
+									{errors.email && <p className="text-red-500">{errors.email}</p>}
 								</div>
 							</div>
-						</form>
+							<div className="w-full md:pt-10">
+								<div className="form-group">
+									<textarea placeholder="Message" className={`border-b border-[#0d2c32] dark:border-[#E7E6E2] text-#0d2c32 dark:text-white ${errors.message ? "border-red-500" : ""}`} name="message" value={modifiedData.message} onChange={handleMessageChange} rows={4} />
+									{errors.message && <p className="text-red-500">{errors.message}</p>}
+								</div>
+							</div>
+							<div className="w-full flex justify-center m-auto">
+								<div className="text-center mt-6">
+									{isSending ? (
+										<div className="loader dark:text-[#c0ccbb]">Sending...</div>
+									) : (
+										<>
+											<ReCAPTCHA className="mb-6" sitekey={process.env.RECAPTCHA_SITE_KEY || ""} onChange={handleRecaptchaChange} />
+											<button type="submit" className="button dark:dark-button cursor-pointer">
+												Send message
+											</button>
+											{errorContact && <p className="text-red-500">{errorContact.message}</p>}
+										</>
+									)}
+								</div>
+							</div>
+						</div>
+					</form>
 				</div>
-				
 			</section>
 		</>
 	);
