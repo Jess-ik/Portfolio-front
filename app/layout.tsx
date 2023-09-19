e9a577a5f258ccba216a4377f9e251ce36e9cea6
Jess <jessica.louvel@gmail.com>
2023-09-19 15:07:59 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,49 @@ interface RootLayoutProps {
 	children: React.ReactNode; // Define the type of children
 }
 
+export const metadata = {
+  title: "Jess • Creative + Developer",
+  description: "Portfolio of Jess Louvel - Creative & Developer",
+  keywords: ["portfolio", "creative", "developer", "web design", "web development", "graphic designer"],
+
+  openGraph: {
+    title: "Jess • Creative + Developer",
+    description: "Portfolio of Jess Louvel - Creative & Developer",
+    url: "https://jess-louvel.com",
+    siteName: "Portfolio of Jess Louvel",
+    images: [
+      {
+        url: "https://jess-louvel.com/logo-landing-black.webp",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://jess-louvel.com/logo-landing-black.webp",
+        width: 1800,
+        height: 1600,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Jess • Creative + Developer",
+    description: "Portfolio of Jess Louvel - Creative & Developer",
+    images: ["https://jess-louvel.com/logo-landing-black.webp"],
+  },
+};
+
+const sitedata = {
+  "@context": "https://schema.org/",
+  "@type": "Person",
+  name: "Jess Louvel",
+  url: "https://jess-louvel.com",
+  image: "https://jess-louvel.com/logo-landing-black.webp",
+  jobTitle: "Creative + Web Developer",
+  sameAs: ["https://www.linkedin.com/in/jesslouvel/", "https://github.com/Jess-ik", "https://www.instagram.com/jess_louvel/"],
+};
+
 export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang="en" suppressHydrationWarning>
