28956a12e22fd9561143319902c00551003d4e74
Jess <jessica.louvel@gmail.com>
2023-10-24 15:06:39 +0200

    Update page.tsx

--- app/[lang]/projects/[slug]/page.tsx
+++ app/[lang]/projects/[slug]/page.tsx
@@ -8,48 +8,48 @@ import React, { useEffect, useState } from "react";
 
 interface ProjectData {
 	data: {
-	  attributes: {
-		subtitle: string;
-		title: string;
-		heroImage: {
-		  data: {
-			attributes: {
-			  url: string;
-			  alternativeText: string;
+		attributes: {
+			subtitle: string;
+			title: string;
+			heroImage: {
+				data: {
+					attributes: {
+						url: string;
+						alternativeText: string;
+					};
+				};
 			};
-		  };
-		};
-		category: string;
-		tools: {
-		  data: {
-			id: string;
-			attributes: {
-			  iconShort: string;
-			  iconName: string;
+			category: string;
+			tools: {
+				data: {
+					id: string;
+					attributes: {
+						iconShort: string;
+						iconName: string;
+					};
+				}[];
 			};
-		  }[];
-		};
-		link1?: string;
-		link2?: string;
-		link1Name?: string;
-		link2Name?: string;
-		description: string;
-		gallery: {
-		  data: {
-			id: string;
-			attributes: {
-			  url: string;
-			  alternativeText: string;
+			link1?: string;
+			link2?: string;
+			link1Name?: string;
+			link2Name?: string;
+			description: string;
+			gallery: {
+				data: {
+					id: string;
+					attributes: {
+						url: string;
+						alternativeText: string;
+					};
+				}[];
 			};
-		  }[];
 		};
-	  };
 	};
-  }
-  
+}
+
 type Params = {
 	params: {
-		lang: string
+		lang: string;
 		slug: string;
 	};
 };
@@ -58,29 +58,25 @@ export default function ProjectDetails({ params }: Params) {
 	const { lang, slug } = params; // Extract the slug
 	const [data, setData] = useState<ProjectData | null>(null); // Initial state with correct type
 
-  useEffect(() => {
-    const fetcher = async (url: string) => {
-      const response = await fetch(url);
-      return response.json();
-	};
-	  
-	const apiUrl =
-	lang === "en"
-	  ? `${process.env.API_URL}/projects/${slug}`
-	  : `${process.env.API_URL}/projects/${slug}-fr`;
+	useEffect(() => {
+		const fetcher = async (url: string) => {
+			const response = await fetch(url);
+			return response.json();
+		};
 
-	
-    const fetchData = async () => {
-      try {
-        const result = await fetcher(apiUrl);
-        setData(result); // Met à jour l'état avec les données récupérées
-      } catch (error) {
-        console.error("Erreur lors du chargement des données", error);
-      }
-    };
+		const apiUrl = lang === "en" ? `${process.env.API_URL}/projects/${slug}` : `${process.env.API_URL}/projects/${slug}-fr`;
 
-    fetchData(); // Appel de la fonction pour récupérer les données au montage du composant
-  }, [slug]); // Le tableau vide signifie que cela ne s'exécute qu'au montage
+		const fetchData = async () => {
+			try {
+				const result = await fetcher(apiUrl);
+				setData(result); // Met à jour l'état avec les données récupérées
+			} catch (error) {
+				console.error("Erreur lors du chargement des données", error);
+			}
+		};
+
+		fetchData(); // Appel de la fonction pour récupérer les données au montage du composant
+	}, [slug]); // Le tableau vide signifie que cela ne s'exécute qu'au montage
 
 	return (
 		<>
@@ -88,7 +84,7 @@ export default function ProjectDetails({ params }: Params) {
 			<section className={`detail-hero w-full px:2 pt-24 md:pt-32 `}>
 				<div className="max-w-screen-2xl m-auto pt-20 px-6 md:px-10 lg:px-16 xl:px-32">
 					<Link className="flex flex-nowrap items-center gap-2 dark:text-[#b0b0b0]" href="/projects" aria-label="Go back to all projects">
-						<BsArrowLeft /> Back to all projects
+						<BsArrowLeft /> {lang === "en" ? "Back to all projects" : "Retour aux projets"}
 					</Link>
 
 					<h5 className={`yeseva dark:text-[#c0ccbb] pt-6`}>{data?.data.attributes.subtitle}</h5>
@@ -98,7 +94,7 @@ export default function ProjectDetails({ params }: Params) {
 
 					<ul className="py-10 flex flex-col md:flex-row flex-wrap justify-between dark:text-[#e7e6e2]">
 						<li>
-							<span>Category</span>
+							<span>{lang === "en" ? "Category" : "Catégorie"}</span>
 							<p className="dark:text-[#b0b0b0]">{data?.data.attributes.category}</p>
 						</li>
 						<li className="flex flex-col  pt-6 md:pt-0">
@@ -111,7 +107,7 @@ export default function ProjectDetails({ params }: Params) {
 						</li>
 						{data?.data.attributes.link1 || data?.data.attributes.link2 ? (
 							<li className="py-12 md:py-0 ">
-								<span>Links</span>
+								<span>{lang === "en" ? "Links" : "Liens"}</span>
 								<div className="links flex gap-10 cursor-pointer mt-2.5 ">
 									{data?.data.attributes.link1 && (
 										<a href={data?.data.attributes.link1} target="_blank" rel="noopener noreferrer">
@@ -136,18 +132,16 @@ export default function ProjectDetails({ params }: Params) {
 					<div className="lg:w-4/12">
 						<div className="flex flex-col lg:flex-row lg:items-baseline">
 							<div className="lg:w-10/12">
-								<h4 className="dark:text-[#E7E6E2]">Description</h4>
+								<h4 className="dark:text-[#E7E6E2]">{lang === "en" ? "Description" : "Déscription"}</h4>
 							</div>
 						</div>
 					</div>
 					<div className=" lg:w-8/12 mt-10 lg:mt-0 mb-30">
-					<div className="text">
-  {data && ( // Vérifiez si data est défini
-    <ReactMarkdown className="dark:text-[#b0b0b0]">
-      {data.data.attributes.description}
-    </ReactMarkdown>
-  )}
-</div>
+						<div className="text">
+							{data && ( // Vérifiez si data est défini
+								<ReactMarkdown className="dark:text-[#b0b0b0]">{data.data.attributes.description}</ReactMarkdown>
+							)}
+						</div>
 					</div>
 				</div>
 			</section>
