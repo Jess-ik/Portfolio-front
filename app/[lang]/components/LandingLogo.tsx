93f1721b111e0431ec677ebd5d1f45df9b299179
Jess <jessica.louvel@gmail.com>
2023-10-24 13:39:49 +0200

    fix public img

--- app/[lang]/components/LandingLogo.tsx
+++ app/[lang]/components/LandingLogo.tsx
@@ -18,9 +18,9 @@ export default function LandingLogo() {
 		<div className="logo-hero">
 			<h1 className='hidden'>Jess Louvel - Creative + Developer</h1>
 				{resolvedTheme === 'dark' ? (
-        <img data-swiper-parallax="-2000" src="./logo-landing.webp" alt="Logo of Jess Louvel in white" width={1094} height={800}/>
+        <img data-swiper-parallax="-2000" src="https://strapi-h2ev.onrender.com/uploads/logo_landing_9a50b372c9.webp" alt="Logo of Jess Louvel in white" width={1094} height={800}/>
       ) : (
-        <img data-swiper-parallax="-2000" src="./logo-landing-black.webp" alt="Logo of Jess Louvel in black" width={1094} height={800}/>
+        <img data-swiper-parallax="-2000" src="https://strapi-h2ev.onrender.com/uploads/logo_landing_black_f5f51722be.webp" alt="Logo of Jess Louvel in black" width={1094} height={800}/>
       )}
 		</div>
 	);
--- app/[lang]/components/NavLogo.tsx
+++ app/[lang]/components/NavLogo.tsx
@@ -22,9 +22,9 @@ const NavLogo = () => {
    
     >
       {resolvedTheme === 'dark' ? (
-        <img className="w-12" src="/logo-light.png" alt="Monogram of Jess Louvel in white" width={210} height={263}/>
+        <img className="w-12" src="https://strapi-h2ev.onrender.com/uploads/logo_light_7ea0844142.png" alt="Monogram of Jess Louvel in white" width={210} height={263}/>
       ) : (
-        <img className="w-12" src="./logo-black.png" alt="Monogram of Jess Louvel in black" width={210} height={263}/>
+        <img className="w-12" src="https://strapi-h2ev.onrender.com/uploads/logo_black_b0de72b040.png" alt="Monogram of Jess Louvel in black" width={210} height={263}/>
       )}
     </button>
   )
