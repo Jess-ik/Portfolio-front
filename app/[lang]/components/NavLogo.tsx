5960a231953ac309aa3ba1708e8794558718b789
Jess <jessica.louvel@gmail.com>
2023-11-02 10:47:08 +0100

    edit monogram

--- app/[lang]/components/NavLogo.tsx
+++ app/[lang]/components/NavLogo.tsx
@@ -22,9 +22,9 @@ const NavLogo = () => {
    
     >
       {resolvedTheme === 'dark' ? (
-        <img className="w-12" src="https://strapi-h2ev.onrender.com/uploads/logo_light_7ea0844142.png" alt="Monogram of Jess Louvel in white" width={210} height={263}/>
+        <img className="w-12" src="https://strapi-h2ev.onrender.com/uploads/logo_light_ea5a2fd898.webp" alt="Monogram of Jess Louvel in white" width={210} height={263}/>
       ) : (
-        <img className="w-12" src="https://strapi-h2ev.onrender.com/uploads/logo_black_b0de72b040.png" alt="Monogram of Jess Louvel in black" width={210} height={263}/>
+        <img className="w-12" src="https://strapi-h2ev.onrender.com/uploads/logo_black_9fd6d51de8.webp" alt="Monogram of Jess Louvel in black" width={210} height={263}/>
       )}
     </button>
   )
