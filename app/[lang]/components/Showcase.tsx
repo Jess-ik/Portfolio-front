209a57dafe593b0903211780122cf5b0635af3b6
Jess <jessica.louvel@gmail.com>
2023-11-02 10:35:10 +0100

    add portfolio project link

--- app/[lang]/components/Showcase.tsx
+++ app/[lang]/components/Showcase.tsx
@@ -101,25 +101,26 @@ export default function App() {
 					>
 						{/* LANDING SLIDE */}
 						<SwiperSlide className="h-full slide" data-swiper-autoplay="3000">
-							<div className="blob">
-								<svg viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" xmlns-xlink="http://www.w3.org/1999/xlink" width="100%" id="blobSvg">
-									<defs>
-										<linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-											<stop offset="10%" stopColor="#32DFC0">
-												<animate attributeName="stop-color" values="#32DFC0; #C585F8; #32DFC0" dur="4s" repeatCount="indefinite"></animate>
-											</stop>
+							<Link href={`/projects/jess-louvel`} as={`/projects/jess-louvel`}>
+								<div className="blob">
+									<svg viewBox="0 0 500 500" xmlns="http://www.w3.org/2000/svg" xmlns-xlink="http://www.w3.org/1999/xlink" width="100%" id="blobSvg">
+										<defs>
+											<linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+												<stop offset="10%" stopColor="#32DFC0">
+													<animate attributeName="stop-color" values="#32DFC0; #C585F8; #32DFC0" dur="4s" repeatCount="indefinite"></animate>
+												</stop>
 
-											<stop offset="90%" stopColor="#C585F8">
-												<animate attributeName="stop-color" values="#C585F8; #32DFC0; #C585F8" dur="5s" repeatCount="indefinite"></animate>
-											</stop>
-										</linearGradient>
-									</defs>
-									<path d="M439.5,309.5Q413,369,361.5,401.5Q310,434,251.5,430.5Q193,427,159.5,383.5Q126,340,105.5,295Q85,250,78.5,185Q72,120,131,93.5Q190,67,255.5,49.5Q321,32,367.5,81.5Q414,131,440,190.5Q466,250,439.5,309.5Z" id="blob" fill="url(#gradient)">
-										<animate
-											attributeName="d"
-											dur="15s"
-											repeatCount="indefinite"
-											values="
+												<stop offset="90%" stopColor="#C585F8">
+													<animate attributeName="stop-color" values="#C585F8; #32DFC0; #C585F8" dur="5s" repeatCount="indefinite"></animate>
+												</stop>
+											</linearGradient>
+										</defs>
+										<path d="M439.5,309.5Q413,369,361.5,401.5Q310,434,251.5,430.5Q193,427,159.5,383.5Q126,340,105.5,295Q85,250,78.5,185Q72,120,131,93.5Q190,67,255.5,49.5Q321,32,367.5,81.5Q414,131,440,190.5Q466,250,439.5,309.5Z" id="blob" fill="url(#gradient)">
+											<animate
+												attributeName="d"
+												dur="15s"
+												repeatCount="indefinite"
+												values="
     M439.5,309.5Q413,369,361.5,401.5Q310,434,251.5,430.5Q193,427,159.5,383.5Q126,340,105.5,295Q85,250,78.5,185Q72,120,131,93.5Q190,67,255.5,49.5Q321,32,367.5,81.5Q414,131,440,190.5Q466,250,439.5,309.5;
     M455.5,311.5Q420,373,367.5,411.5Q315,450,257.5,427Q200,404,142.5,387Q85,370,82,310Q79,250,91.5,197Q104,144,142,90Q180,36,247,44Q314,52,357,96.5Q400,141,445.5,195.5Q491,250,455.5,311.5;
     M406.5,307Q406,364,357.5,398Q309,432,252,426Q195,420,133,400Q71,380,42.5,315Q14,250,32,177.5Q50,105,116.5,74.5Q183,44,250.5,42Q318,40,383,73Q448,106,427.5,178Q407,250,406.5,307;
@@ -127,11 +128,12 @@ export default function App() {
     M417.5,309.5Q413,369,368.5,422.5Q324,476,249,480Q174,484,149,413Q124,342,86,296Q48,250,83.5,202.5Q119,155,159.5,126Q200,97,260.5,63.5Q321,30,372.5,76.5Q424,123,423,186.5Q422,250,417.5,309.5;
     M439.5,309.5Q413,369,361.5,401.5Q310,434,251.5,430.5Q193,427,159.5,383.5Q126,340,105.5,295Q85,250,78.5,185Q72,120,131,93.5Q190,67,255.5,49.5Q321,32,367.5,81.5Q414,131,440,190.5Q466,250,439.5,309.5;
   "
-										/>
-									</path>
-								</svg>
-							</div>
-							<LandingLogo />
+											/>
+										</path>
+									</svg>
+								</div>
+								<LandingLogo />
+							</Link>
 						</SwiperSlide>
 
 						{/* SHOWCASE PROJECTS SLIDES FROM STRAPI */}
